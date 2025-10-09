c6184c0975a6909486165dc5b5f7e4c6dd92d57c
0xvoidmain <hoangtung.utc@gmail.com>
2025-10-09 16:30:35 +0700

    Update webapp

---
+++ assets/concat-hex-stkqDwKD.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-Atmi8JGg.js";function n(c){return o(...c)}export{n as concatHex};
---
+++ assets/getBalance-BQBkUIDV.js
@@ -0,0 +1,1 @@
+import{z as r,A as t}from"./index-Atmi8JGg.js";import{getCurrencyMetadata as n}from"./getCurrencyMetadata-H1dnIhhx.js";async function s(a){const[e,c]=await Promise.all([r(a),n(a)]);return{...c,chainId:a.contract.chain.id,displayValue:t(e,c.decimals),tokenAddress:a.contract.address,value:e}}export{s as getBalance};
